683e6402e35e849507e890083887bbb2d98f9898
hacker2108-maker <profesorhacker2108@gmail.com>
2025-03-23 01:13:31 -0400

    Add files via upload

---
+++ Install.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python3
+import os
+import time
+import sys
+
+os.system("clear")
+print('''CREATED BY Hacker2108
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+
+print (''' \033[95m
++--------------------------------------+
+| This Tool Install All Basic Packages on your Termux |
++--------------------------------------+
+| Coded By Hacker2108|version : 3.0  |
++--------------------------------------+''')
+
+slowprint(''' \033[93m
+[01] python
+[02] python2
+[03] python-dev
+[04] python3
+[05] php
+[06] java
+[07] git
+[08] perl
+[09] bash
+[10] nano
+[11] curl
+[12] openssl
+[13] openssh
+[14] wget
+[15] clang
+[16] nmap
+[17] w3m
+[18] hydra
+[19] ruby
+[20] macchanger
+[21] host
+[22] dnsutils
+[23] coreutils
+[24] fish
+[25] zip
+[27] tor
+[28] hydra
+[29] figlet 
+[30] cowsay
+[31] tar
+[32] unzip
+[33] vim
+[34] ruby
+[35] wcalc
+[36] bmon
+[37] unrar
+[38] proot
+[39] golang''')
+slowprint('''\033[96m
+This Command for access Storage in Termux
+[00] termux-setup-storage''')
+print ("                                            ")
+choice = input("\033[93mDo You Want to Install All Packages [y/n] : ")
+if choice == 'n' : sys.exit()
+if choice == 'y' : os.system ("apt update")
+os.system ("apt upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash")
+
+print ("wait for second and start hacking")
+
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+os.system("apt install metasploit-framework -y")
+os.system("apt install aircrack-ng -y")
+os.system("apt install termux-tools -y")
+os.system("apt install cronie -y")
+os.system("apt install parallel -y")
+os.system("apt install radare2 -y")
+os.system("apt install openjdk-17 -y")
+os.system("apt install libxml2 -y")
+os.system("apt install libxslt -y")
+os.system("apt install tmate -y")
+os.system("apt install proxychains-ng -y")
+os.system("apt install texinfo -y")
+os.system("apt install wireshark-gtk -y")
+os.system("apt install rlwrap -y")
+os.system("apt install binutils -y")
+os.system("apt install ripgrep -y")
+os.system("apt install hashdeep -y")
+os.system("apt install nasm -y")
+os.system("apt install openssh-server -y")
+os.system("apt install gh -y")  # GitHub CLI
+os.system("apt install fzf -y")  # Fuzzy finder
+os.system("apt install mosh -y")  # Mobile shell
+os.system("apt install termux-services -y")
+os.system("apt install tracepath -y")
+os.system("apt install sl -y")  # Steam Locomotive
+os.system("apt install fortune -y")
+os.system("apt install asciinema -y")  # Terminal recorder
+os.system("apt install kubectl -y")
+os.system("apt install hugo -y")  # Static site generator
+os.system("apt install youtube-dl -y")
+
+print ("""
+subscribe Nitro Hacker""")
+
+os.system ("apt install ruby -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+os.system ("apt install fish -y")
+os.system ("apt install zip -y")
+os.system ("apt install hydra -y")
+os.system ("apt install figlet -y")
+os.system ("apt install cowsay -y")
+os.system ("apt install unzip -y")
+os.system ("apt install vim -y")
+os.system ("apt install ruby -y")
+os.system ("apt install wcalc -y")
+os.system ("apt install bmon -y")
+os.system ("apt install unrar -y")
+os.system ("apt install proot -y")
+os.system ("apt install golang -y")
+print ("Allow the Button For Access the Storage in Termux")
+
+
+os.system ("termux-setup-storage")
+  
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+print("\033[95m+-------------------------------------------------+")
+slowprint('''\033[95m|             Welcome To HACKER2108           |
+|           Subscribe Our YouTube Channel         |
+| Watch Ours Tutorials For Learn Ethical Hacking |''')
+print("+-------------------------------------------------+")
+
+
+input("\n\nPress the enter key to exit : ")
